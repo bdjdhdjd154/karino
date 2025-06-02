17be9804091c38eb398eedf42d92280b4104434f
mobinaGhasemi <mobinaen492@gmail.com>
2025-06-02 21:19:50 +0330

    reg5

--- main/views.py
+++ main/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
-
+from rest_framework.decorators import api_view
+from django.views.decorators.csrf import csrf_exempt
 # Create your views here.
 
 def home(request):
-   return render(request,'firstpage.html')
+   return render(request,'firstpage.html'),
+@csrf_exempt
+@api_view(["POST"])
+def myregister_view(request):
+   return render(request, 'main/register.html')
+   
