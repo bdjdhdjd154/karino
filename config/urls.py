9e4db7651c5a03885d58939b0cdb73b9961a1400
mobinaGhasemi <mobinaen492@gmail.com>
2025-06-02 21:22:01 +0330

    reg9

--- config/urls.py
+++ config/urls.py
@@ -1,50 +1,19 @@
-"""
-URL configuration for config project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
-from profiles.views import CustomAuthToken
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework.authtoken.views import obtain_auth_token
-from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView, TokenRefreshView
-from django.conf import settings
-from django.conf.urls.static import static
-from main import views
-
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from profiles.views import CustomAuthToken
 
 urlpatterns = [
-    
     path('admin/', admin.site.urls),
-    path('api/', include('profiles.urls')),
-    path('api-token-auth/', obtain_auth_token, name='api_token_auth'),
-    path('api/', include('profiles.urls')),
-    path('api-token-auth/', obtain_auth_token, name='api_token_auth'),
-    path('admin',admin.site.urls),
-    path('api/login/', obtain_auth_token, name='api_token_auth'),
-    path('login/',CustomAuthToken.as_view(), name='login'),
-    path('api/profile/', include('profiles.urls')),
-    path('api/', include('profiles.urls')),
-    path('api/register', TokenRefreshView.as_view(), name='register'),
-    path('api/login', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('api/token/refresh', TokenRefreshView.as_view(), name='token_refresh'),
-    ##path('api/', include('company.urls')),
-    ##path('api/company/', include('company.urls')),
-    path('api/profiles/', include('profiles.urls')),
-    ##path('api/company/', include('company.urls')), 
-    path('', include('main.urls')),
-
+    path('api/', include('profiles.urls')),  # فقط یکبار کافیه
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('api-token-auth/', obtain_auth_token, name='api_token_auth'),  # Token auth
+    path('login/', CustomAuthToken.as_view(), name='login'),  # Custom login
+    path('', include('main.urls')),  # فرانت‌اند و صفحات HTML
 ]
+
+
 ##if settings.DEBUG:
     ##urlpatterns+= static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
