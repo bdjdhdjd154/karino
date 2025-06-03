38e5a4ad3429c437797a68389adb0dac14404cfb
mobinaGhasemi <mobinaen492@gmail.com>
2025-06-03 15:53:33 +0330

    login5

--- main/urls.py
+++ main/urls.py
@@ -3,5 +3,6 @@ from main.views import home
 from . import views
 urlpatterns=[
     path('', home, name='home'),
-    path('register/', views.myregister_view, name='register')
+    path('register/', views.myregister_view, name='register'),
+    path('login/', views.mylogin_view, name='login' )
 ]
