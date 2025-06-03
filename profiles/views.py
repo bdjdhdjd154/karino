66685722e90b9f725095b584fd972fef42ceec29
mobinaGhasemi <mobinaen492@gmail.com>
2025-06-03 15:50:33 +0330

    login1

--- profiles/views.py
+++ profiles/views.py
@@ -19,6 +19,7 @@ from rest_framework import status
 from rest_framework.decorators import api_view
 from django.views.decorators.csrf import csrf_exempt
 from rest_framework.decorators import api_view, permission_classes
+from django.contrib.auth import authenticate
 
 
 class ProfileListCreateView(APIView):
@@ -44,11 +45,19 @@ def register_view(request):
     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
-class CustomAuthToken(ObtainAuthToken):
-    def post(self, request, *args, **kwargs):
-        response=super().post(request, *args, **kwargs)
-        token=Token.objects.get(key=response.data['token'])
-        return Response({'token':token.key})
+@api_view(["POST"])
+def login_api_view(request):
+    username = request.data.get("username")
+    password = request.data.get("password")
+
+    user = authenticate(username=username, password=password)
+
+    if user is not None:
+        token, created = Token.objects.get_or_create(user=user)
+        return Response({"token": token.key}, status=200)
+    return Response({"detail": "نام کاربری یا رمز عبور اشتباه است"}, status=status.HTTP_401_UNAUTHORIZED)
+
+
 class MyProfileView(APIView):
     permission_classes = [IsAuthenticated]
 
