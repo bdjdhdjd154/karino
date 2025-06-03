0b4bcd6503ae81c9a82916443c26268a4b76837d
mobinaGhasemi <mobinaen492@gmail.com>
2025-06-03 15:52:59 +0330

    login4

--- main/views.py
+++ main/views.py
@@ -4,9 +4,11 @@ from django.views.decorators.csrf import csrf_exempt
 # Create your views here.
 
 def home(request):
-   return render(request,'firstpage.html'),
+   return render(request,'firstpage.html')
+def mylogin_view(request):
+   return render(request, 'main/login.html')
+   
 @csrf_exempt
 @api_view(["POST"])
 def myregister_view(request):
    return render(request, 'main/register.html')
-   
