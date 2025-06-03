de9d30c3e776a076738cdaa22c70cf9b745f1844
mobinaGhasemi <mobinaen492@gmail.com>
2025-06-03 15:54:06 +0330

    login6

--- profiles/urls.py
+++ profiles/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from . import views
+from .views import login_api_view
 
 urlpatterns = [
     path('register/', views.register_view, name='register'),  # 🔹 توجه: class-based
@@ -7,4 +8,5 @@ urlpatterns = [
     path('profile/', views.ProfileDetailView.as_view(), name='profile-detail'),
     path('profile/update/', views.ProfileUpdateView.as_view(), name='profile-update'),  # جداش کن
     path('logout/', views.LogoutView.as_view(), name='logout'),
+    path('login/', views.login_api_view, name='api-login')
 ]
