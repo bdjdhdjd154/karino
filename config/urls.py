606b91e150f4c6904ef358652f0ca477dff3c530
mobinaGhasemi <mobinaen492@gmail.com>
2025-06-03 15:54:33 +0330

    login7

--- config/urls.py
+++ config/urls.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from django.urls import path, include
 from rest_framework.authtoken.views import obtain_auth_token
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
-from profiles.views import CustomAuthToken
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -10,7 +10,7 @@ urlpatterns = [
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('api-token-auth/', obtain_auth_token, name='api_token_auth'),  # Token auth
-    path('login/', CustomAuthToken.as_view(), name='login'),  # Custom login
+  
     path('', include('main.urls')),  # فرانت‌اند و صفحات HTML
 ]
 
